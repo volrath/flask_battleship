8d2ca1d2cf54e13f412d5417d9a2f4c4f24c5eae
Daniel Barreto <dbarreto@talpor.com>
2012-07-01 12:51:02 -0430

    added gevent_socketio magic to the table. only downside is we now have to use `SocketIOServer` which does not shines as the werkzeug server.
    imported our `BattleShipNamespace` and serving it into the default
    namespace.

--- start.py
+++ start.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
+from gevent import monkey; monkey.patch_all()
+from socketio import socketio_manage
+
+from battleship import BattleShipNamespace
+
 
 @app.route('/')
 def battleship():
     return render_template('battleship.html')
 
+@app.route('/socket.io/<path:path>')
+def socket_io(path):
+    socketio_manage(request.environ, {'': BattleShipNamespace})
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    if app.debug:
+        from werkzeug.debug import DebuggedApplication
+        app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
+
+    from socketio.server import SocketIOServer
+    SocketIOServer(('127.0.0.1', 5000), app, namespace="socket.io",
+                   policy_server=False).serve_forever()
