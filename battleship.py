b0182b640eb4548caf7e2c20ef9c112fc136d7d3
Daniel Barreto <dbarreto@talpor.com>
2012-07-01 19:53:31 -0430

    HUGE refactoring to battleship.py.

--- battleship.py
+++ battleship.py
@@ -5,33 +5,38 @@ from socketio.namespace import BaseNamespace
 
 
 class BattleShipRoomMixin(object):
-    NOT_READY = 0  # user's not ready, needs to place ships.
-    READY     = 1  # user's ready to fight.
+    def _find_by_username(self, username):
+        """Finds a socket given a `username`."""
+        try:
+            return [socket
+                    for _, socket in self.socket.server.sockets.iteritems()
+                    if socket.session.get('username') == username][0]
+        except KeyError:
+            return None
 
-    def __init__(self, *args, **kwargs):
-        super(BattleShipNamespace, self).__init__(*args, **kwargs)
-        self.rooms = {}  # {<room_name>: {<username>: <status>, ...}, ...}
-        self.users_sockets = {}  # {<username>: <socket>, ...}
-
-    def _find_room_players(self, room_name):
+    def _find_room_players(self, room_name=None):
         """Finds players in the given `room_name`, returns its sockets
         instances.
         """
+        room_name = room_name or self._room_name
         return [socket for _, socket in self.socket.server.sockets.iteritems()
-                       if getattr(socket.session, 'room', '') == room_name]
+                       if socket.session.get('room', '') == room_name]
 
-    def _get_player(self):
+    def _get_username(self):
         """Returns the username of the current player."""
         return self.session.get('username', '')
-    _player = property(_get_player)
+    _username = property(_get_username)
 
     _other_player = None
     def _get_other_player(self):
-        """Returns the username of the opponent player in the room."""
+        """Returns the socket of the opponent player in the room."""
         if self._other_player is None:
-            usernames = self.rooms[self._room_name].keys()
-            self._other_player= [username for username in usernames
-                                          if username != self._player][0]
+            players = self._find_room_players()
+            return [
+                player
+                for player in players
+                if player.session.get('username') != self._username
+            ][0]
         return self._other_player
     _opponent = property(_get_other_player)
 
@@ -46,28 +51,24 @@ class BattleShipRoomMixin(object):
         """
         # TODO: Restrict rooms to only 2 users per room, and cannot
         #       have the same usernames.
-        self.rooms.setdefault(room_name, {}).update({username: self.NOT_READY})
-        self.users_sockets[username] = self.socket
         self.session.setdefault('username', username)
+        self.session.setdefault('ready', False)
         self.session.setdefault('room', room_name)
 
     def leave(self, room_name):
         """Lets a user leave a room on a specific Namespace."""
         # TODO: improve(build) error messages
         try:
-            self.rooms[room_name].remove(self.session['username'])
-            del self.users_sockets[self.session['username']]
             del self.session['room']
             del self.session['username']
             return True
-        except (AttributeError, KeyError, ValueError):
+        except KeyError:
             return False
 
     def emit_to_room(self, event, room_name=None, *args):
         """Sends the `event` to all in the `room_name` (in this
         particular Namespace).
         """
-        room_name = room_name or self.session.room
         players = self._find_room_players(room_name)
         if not players:
             return
@@ -75,15 +76,18 @@ class BattleShipRoomMixin(object):
         for player in players:
             player.send_packet(pkt)
 
-    def emit_to_user(self, event, username=None, *args, **kwargs):
+    def emit_to_user(self, event, user=None, *args, **kwargs):
         """Works exactly the same as `BaseNamespace.emit`, but
         pre-selects a socket to emit the package given a
-        `username`. If the `username` is not present we assume we're
+        `user`. If the `user` is not present we assume we're
         emiting this package to the current user, and thus we use
         `emit`.
+
+        :user: can be either a username string or a socket instance.
+
         For more info read `BaseNamespace.emit`'s documentation.
         """
-        if username is None and self.session.has_key('username'):
+        if user is None and self.session.has_key('username'):
             return self.emit(event, *args, **kwargs)
         callback = kwargs.pop('callback', None)
         if kwargs:
@@ -98,50 +102,72 @@ class BattleShipRoomMixin(object):
             pkt['ack'] = 'data'
             pkt['id'] = msgid = self.socket._get_next_msgid()
             self.socket._save_ack_callback(msgid, callback)
-        self.users_sockets[username].send_packet(pkt)
+        socket = self._find_by_username(user) if isinstance(user, str) else user
+        socket.send_packet(pkt)  # maybe catch the attributeerror
 
 
 class BattleShipNamespace(BaseNamespace, BattleShipRoomMixin, BroadcastMixin):
     def on_user_enters_room(self, username, room_name):
         self.join(username, room_name)
-        self.broadcast_event('room_participants', self.rooms[room_name])
-        self.emit_to_room(room_name, 'announcement',
-                          '%s has connected' % username)
+        self.broadcast_event('room_participants',
+                             {self._room_name: len(self._find_room_players())})
+        self.emit_to_room('announcement', '%s has connected' % username)
 
     def on_user_is_ready(self):
         """The user has placed all his/her ships on the table and is
         ready to fight.
         """
-        room = self.rooms[self._room_name]
-        room[self._player] = self.READY
-        self.emit_to_room('announcement', '%s is ready' % self._player)
-        if len(room) > 1 and all([player_status == self.READY
-                                  for player_status in room.itervalues()]):
+        self.session['ready'] = True
+        self.emit_to_room('announcement', '%s is ready' % self._username)
+        players = self._find_room_players()
+        if len(players) > 1 and all([player.session['ready']
+                                     for player in players]):
             # We're in business! we will just randomly choose one
             # participant to begin the game.
-            usernames = room.keys()
-            first  = random.choice(usernames)
-            second = [username for username in usernames
-                               if username != first][0]
-            self.emit_to_user(first,  'turn_begins')
-            self.emit_to_user(second, 'turn_ends')
+            if random.choice([True, False]):  # Like a coin to the air
+                first  = self.socket
+                second = self._opponent
+            else:
+                first  = self._opponent
+                second = self.socket
+            self.emit_to_user('turn_begins', first)
+            self.emit_to_user('turn_ends', second)
 
-    def on_user_shoots(self, row, column):
+    def on_user_shoots(self, tile):
         """The user shoots for a tile!"""
+        row, column = tile
         self.emit_to_room('announcement',
-                          '%s shoots for %s %s!' % (self._player,
+                          '%s shoots for %s %s!' % (self._username,
                                                     row, column))
-        self.emit_to_user(self._opponent, 'receive_shot',
-                          {'row': row, 'column': column},
-                          callback=self.ack_shot)
+        # there is no need in passing arguments with this event cause
+        # the tile is referenced by id, and the calculations for a new
+        # state in that clase are made directly into the client.
+        tile_id = 'd-%s-%s' % tuple(tile)
+        self.emit_to_user('/tiles/%s:receive_shot' % tile_id,
+                          self._opponent, {})
 
-    def ack_shot(self, status, tiles_hit):
+    def on_ack_shot(self, ack):
         """Acknowledgement of a shot. Tells the status of a previous
         shot, if it was a hit, a down, or a miss.
+
+        :ack: is a dictionary with two keys:
+        * :new_state: the new state of the aimed tile.
+
+        * :tiles_hit: is a list with (row, column) tuples of all the
+          affected tiles. This will be commonly only one tile, but in
+          case of a Ship shutdown, this list will contain all the
+          tiles of that ship.
         """
-        # dont know who would be self._player and self._opponent
-        # here.
-        self.emit_to_user(self._player, 'shot_result', status, tiles_hit)
+        new_state = ack['new_state']
+        tiles_hit = ack['tiles_hit']
+        for tile in tiles_hit:
+            tile_id = 'a-%s-%s' % tuple(tile)
+            self.emit_to_user('/tiles/%s:ack_shot' % tile_id, self._opponent,
+                              {'state': new_state})
+        if len(tiles_hit) > 1:
+            self.emit_to_room('announcement',
+                              '%s lost a %s-tiles battleship!' % \
+                              (self._opponent, len(tiles_hit)))
         # change turns
-        self.emit_to_user(self._opponent,  'turn_begins')
-        self.emit_to_user(self._player, 'turn_ends')
+        self.emit_to_user('turn_ends', self._opponent)
+        self.emit_to_user('turn_begins')
